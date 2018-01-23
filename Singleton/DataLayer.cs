8879b7430d8bd7308b58b4547b07d19bc1198886
Muharrem Gürgen <muharrem.gurgen@outlook.com>
2018-01-23 21:12:29 +0300

    Final Version

---
+++ Singleton/DataLayer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class DataLayer
+    {
+        private static DataLayer _dataLayer = new DataLayer();
+        private string _connectionString = "Default connection string";
+
+        private DataLayer()
+        {
+            
+        }
+
+        public static DataLayer Instance 
+        { 
+            get
+            {
+                return _dataLayer;
+            }
+        }
+
+        public string ConnectionString 
+        { 
+            get
+            {
+                return _connectionString;
+            }
+            set
+            {
+                _connectionString = value;
+            }
+        }
+    }
+}
---
+++ SingletonRunner/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Singleton;
+
+namespace SingletonRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataLayer dataLayer = DataLayer.Instance;
+            DataLayer dataLayer1 = DataLayer.Instance;
+
+            Console.WriteLine(dataLayer.ConnectionString);
+            Console.WriteLine(dataLayer1.ConnectionString);
+
+            dataLayer.ConnectionString = "New Connection String";
+
+            Console.WriteLine(dataLayer.ConnectionString);
+            Console.WriteLine(dataLayer1.ConnectionString);
+        }
+    }
+}
