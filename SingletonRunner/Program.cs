83f26bc555f925dce9a3640a859092bff28796fa
Muharrem Gürgen <muharrem.gurgen@outlook.com>
2018-01-29 21:29:44 +0300

    Tryout

--- SingletonRunner/Program.cs
+++ SingletonRunner/Program.cs
@@ -8,15 +8,15 @@ namespace SingletonRunner
         static void Main(string[] args)
         {
             DataLayer dataLayer = DataLayer.Instance;
-            DataLayer dataLayer1 = DataLayer.Instance;
+            //DataLayer dataLayer1 = DataLayer.Instance;
 
             Console.WriteLine(dataLayer.ConnectionString);
-            Console.WriteLine(dataLayer1.ConnectionString);
+            //Console.WriteLine(dataLayer1.ConnectionString);
 
             dataLayer.ConnectionString = "New Connection String";
 
             Console.WriteLine(dataLayer.ConnectionString);
-            Console.WriteLine(dataLayer1.ConnectionString);
+            //Console.WriteLine(dataLayer1.ConnectionString);
         }
     }
 }
